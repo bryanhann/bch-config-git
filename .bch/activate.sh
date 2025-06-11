b14c83913e7c7f43e628fd8ddbea4b2eaf319034
Bryan Hann <nobody@nowhere>
2025-06-11 13:03:46 -0230

    Reduce dependancy on [bch:000:*] functions.

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -2,17 +2,18 @@
 
 r=$(dirname $(dirname ${BASH_SOURCE[0]}))
 
+rm -f ~/.config/git
+ln -s ${r}/config.git ~/.config/git
+
 export BCH_CONFIG_GIT__root=${r}
 export BCH_CONFIG_GIT__init=${r}/.bch/activate.sh
 export BCH_CONFIG_GIT__lbin=${r}/.bch/lbin
 export BCH_CONFIG_GIT__bin=${r}/.bch/bin
 export BCH_CONFIG_GIT__lib=${r}/.bch/lib
 
-rm -f ~/.config/git
-ln -s ${r}/config.git ~/.config/git
-
-bch:000:linkall ${r}/.bch/lbin
-
+source ${r}/.bch/init/fn.sh
 source ${r}/.bch/init/init.sh
+
+::lbin:: ${r}/.bch/lbin
 
 unset r
